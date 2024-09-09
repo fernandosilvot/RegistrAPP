6dc3d49f498a7af58355eb8725bb5b48876c8113
Fernando Silva T <fernandosilvot@gmail.com>
2024-09-09 10:00:14 -0300

    Update reset password form with email alert functionality

--- src/app/reset-password/reset-password.page.ts
+++ src/app/reset-password/reset-password.page.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonButton, IonAlert } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.page.html',
   styleUrls: ['./reset-password.page.scss'],
   standalone: true,
-  imports: [IonButton, IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonAlert, IonButton, IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ResetPasswordPage implements OnInit {
+  public alertButtons = [
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
+
+  setResult(ev: any) {
+    console.log(`Dismissed with role: ${ev.detail.role}`);
+  }
+
   email: string = '';
 
   constructor() {}
@@ -20,7 +34,9 @@ export class ResetPasswordPage implements OnInit {
   onResetPassword() {
     if (this.email) {
       console.log('Correo para restablecer contraseña:', this.email);
-      // Lógica para enviar un correo de restablecimiento
+    } else {
+      console.log('Debe ingresar un correo para restablecer la contraseña');
+      // Show alert
     }
   }
 }
